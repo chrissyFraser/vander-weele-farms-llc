bfec20773b5637afc506a43dcdaeb6ceb0dccd22
Joshua LaForest <josh.laforest38@gmail.com>
2022-10-04 13:14:54 -0800

    created monolith service  and dockerized

--- monolith_service/main.py
+++ monolith_service/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        os.environ.get("CORS_HOST", "http://localhost:5000")
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def root():
+@app.get("/is_working")
+def is_working():
     return {"message": "Hello World"}
