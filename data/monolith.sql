52f94fd751468beaf76e94ddc4aa04f3eb4bfb8a
Joshua LaForest <josh.laforest38@gmail.com>
2022-10-04 15:08:12 -0800

    sql file dockerized but possibly not correctly, table built in the file anyways

---
+++ data/monolith.sql
@@ -0,0 +1,9 @@
+CREATE TABLE produce(
+    id SERIAL NOT NULL UNIQUE,
+    product_name TEXT NOT NULL,
+    picture_file TEXT,
+    available BOOLEAN NOT NULL,
+    height INTEGER NOT NULL,
+    length INTEGER NOT NULL,
+    width INTEGER NOT NULL,
+);
