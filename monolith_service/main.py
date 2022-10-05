5f4a891062c27296ea8f4ed33248e078ef7edf5a
Joshua LaForest <josh.laforest38@gmail.com>
2022-10-05 12:45:06 -0800

    figuring out the file system

--- monolith_service/main.py
+++ monolith_service/main.py
@@ -4,15 +4,6 @@ import os
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        os.environ.get("CORS_HOST", "http://localhost:5000")
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/is_working")
 def is_working():
