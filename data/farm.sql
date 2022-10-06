c6e08d8bced88094f9176e50de87a82be2915118
Joshua LaForest <josh.laforest38@gmail.com>
2022-10-06 15:28:12 -0800

    only doing this so I can get other data

--- data/farm.sql
+++ data/farm.sql
@@ -8,4 +8,7 @@ CREATE TABLE produce(
     height INTEGER NOT NULL,
     length INTEGER NOT NULL,
     width INTEGER NOT NULL
-)
+);
+
+-- INSERT INTO produce Values
+-- (1, 'Potatoes', null, true, 10, 20, 15);
