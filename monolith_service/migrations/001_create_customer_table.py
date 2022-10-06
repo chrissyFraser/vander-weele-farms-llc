ac11ad2b0ca049c24fcdeb6070e431fc9a55c7e9
meganehrlich <meganehrlic4@gmail.com>
2022-10-06 14:20:31 -0700

    Updating

---
+++ monolith_service/migrations/001_create_customer_table.py
@@ -0,0 +1,17 @@
+steps = [
+    [
+        """
+        CREATE TABLE customer (
+            id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+            customer_name VARCHAR(1000) NOT NULL,
+            customer_address VARCHAR(1000),
+            customer_email VARCHAR(500),
+            driver_id INTEGER,
+            priority_id INTEGER
+        );
+        """,
+        """
+        DROP TABLE customer;
+        """
+    ]
+]
