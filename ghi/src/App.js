0c5f4e023cb9d6f856ee962e1fef4c0453770850
Joshua LaForest <josh.laforest38@gmail.com>
2022-10-12 14:48:48 -0800

    get produce react working bug fixes on the wayl

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,4 +1,5 @@
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, NavLink, Routes, Route } from 'react-router-dom';
+
 import { useEffect, useState } from 'react';
 import Construct from './Construct.js'
 import ErrorNotification from './ErrorNotification';
@@ -8,40 +9,67 @@ import ProduceList from './ProduceList';
 function App() {
   const [launch_info, setLaunchInfo] = useState([]);
   const [error, setError] = useState(null);  
+  const [get_all_produce, setProduce] = useState([]);
 
   useEffect(() => {
     async function getData() {
-      let url = `${process.env.REACT_APP_API_HOST}api/launch-details`;
-      console.log('fastapi url: ', url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
+      // let url = `${process.env.REACT_APP_API_HOST}api/launch-details`;
+      // console.log('fastapi url: ', url);
+      // let response = await fetch(url);
+      // console.log("------- hello? -------");
+      // let data = await response.json();
 
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
+      // if (response.ok) {
+      //   console.log("got launch data!");
+      //   setLaunchInfo(data.launch_details);
+      // } else {
+      //   console.log("drat! something happened");
+      //   setError(data.message);
+      // }
+
+
+      
+      let url = `${process.env.REACT_APP_API_HOST_MONOLITH}/api/produce/`;
+      console.log("url", url)
+      let response = await fetch(url);
+      let data = await response.json();
+      if(response.ok){
+        console.log("got produce data")
+        setProduce(data)
+        // console.log(data)
+      }else {
         console.log("drat! something happened");
         setError(data.message);
       }
+
+
     }
     getData();
   }, [])
 
-
+  const domain = /https:\/\/[^/]+/;
+  const basename = process.env.PUBLIC_URL.replace(domain, '');
   return (
-    <>
-    <div>
-      <ErrorNotification error={error} />
-      <Construct info={launch_info} />
-    </div>
-    <BrowserRouter>
-      <Routes>
-        <Route path = "/produce-admin/" element = {<ProduceList />} />
-      </Routes>
-    </BrowserRouter>
-    </>
+      <div>
+        {/* <ErrorNotification error={error} />
+        <Construct info={launch_info} /> */}
+        <BrowserRouter basename={basename}>
+          <div className="container">
+            <div className="tabs is-centered">
+              <ul>
+                <li><NavLink to="/produce-admin">Produce</NavLink></li>
+              </ul>
+            </div>
+            <ErrorNotification error={error} />
+            <Routes>
+              <Route path="/produce-admin" element={<ProduceList get_all_produce={get_all_produce} />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
+      
   );
 }
 
 export default App;
+// <ProduceList produce={produce} />
--- ghi/src/ProduceList.js
+++ ghi/src/ProduceList.js
@@ -1,7 +1,38 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 
-function ProduceList(){
 
-}
+function ProduceList(props){
+    console.log(props)
+    console.log("firing")
+    return(
+        // <h1> Is this Working </h1>
+        <div className="columns is-centered">
+        <div className="column is-narrow">
+            <table className="table is-striped">
+            <thead>
+                <tr>
+                <th>Name</th>
+                <th>Available</th>
+                <th>Picture</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.get_all_produce.map(produce => (
+                <tr key={produce.id}>
+                    <td>{produce.product_name}
+                    </td>
+                    <td className="has-text-centered">
+                    {produce.available ? 'Yes' : 'No'}
+                    </td>
+                    <td>
+                    {produce.picture_file}
+                    </td>
+                </tr>
+                ))}
+            </tbody>
+            </table>
+        </div>
+        </div>
+)}
 export default ProduceList
