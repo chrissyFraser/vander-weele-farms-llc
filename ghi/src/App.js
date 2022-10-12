cdc6959e8d528eceb4420284ef3054bf1f6467ef
Chrissy Fraser <plum5152@gmail.com>
2022-10-11 17:03:49 -0700

    feat - front-end working cart

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,38 +1,99 @@
 import { useEffect, useState } from 'react';
 import Construct from './Construct.js'
 import ErrorNotification from './ErrorNotification';
+import Header from './components/Header';
+import Main from './components/Main.js';
+import Basket from './components/Basket.js';
 import './App.css';
+import data from './produce_data_test.js'
 
 function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);  
+  const {produce} = data;
 
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_API_HOST}api/launch-details`;
-      console.log('fastapi url: ', url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (produce) => {
+    const exist = cartItems.find((x) => x.id === produce.id);
+    if(exist) {
+      setCartItems(
+        cartItems.map((x) => 
+          x.id === produce.id ? {...exist, qty: exist.qty + 1} : x
+          )
+        );
+    } else {
+        setCartItems([...cartItems, {...produce, qty: 1}]);
 
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
     }
-    getData();
-  }, [])
-
-
-  return (
+  };  
+  const onRemove = (produce) => {
+    const exist = cartItems.find((x) => x.id === produce.id);
+    if(exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== produce.id))
+    } else {
+      setCartItems(
+        cartItems.map((x) => 
+          x.id === produce.id ? {...exist, qty: exist.qty - 1} : x
+          )
+      )
+    }
+  }
+  return <>
     <div>
-      <ErrorNotification error={error} />
-      <Construct info={launch_info} />
+      <Header countCartItems={cartItems.reduce((a,v) => a = a + v.qty, 0)}></Header>
+      <div className="row">
+        <Main onAdd={onAdd} produce={produce}/>
+        <Basket
+        onAdd={onAdd} 
+        onRemove={onRemove}  
+        cartItems={cartItems}>
+        </Basket>
+      </div>
     </div>
-  );
+  </>
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   const [launch_info, setLaunchInfo] = useState([]);
+//   const [error, setError] = useState(null);  
+
+//   useEffect(() => {
+//     async function getData() {
+//       let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
+//       console.log('fastapi url: ', url);
+//       let response = await fetch(url);
+//       console.log("------- hello? -------");
+//       let data = await response.json();
+
+//       if (response.ok) {
+//         console.log("got launch data!");
+//         setLaunchInfo(data.launch_details);
+//       } else {
+//         console.log("drat! something happened");
+//         setError(data.message);
+//       }
+//     }
+//     getData();
+//   }, [])
+
+
+//   return (
+//     <div>
+//       <ErrorNotification error={error} />
+//       <Construct info={launch_info} />
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ ghi/src/produce_data_test.js
@@ -0,0 +1,33 @@
+const data = {
+    produce: [
+        {
+            id: 1,
+            product_name: "Lettuce",
+            picture_file: "https://cdn.britannica.com/77/170677-050-F7333D51/lettuce.jpg?w=400&h=300&c=crop",
+            available: true,
+            height: 5,
+            length: 5,
+            width: 5
+        },
+        {
+            id: 2,
+            product_name: "Potatoes",
+            picture_file: "https://media.istockphoto.com/photos/three-potatoes-picture-id157430678?k=20&m=157430678&s=612x612&w=0&h=dfYLuPYwA50ojI90hZ4jCgKZd5TGnqf24UCVBszoZIA=",
+            available: true,
+            height: 10,
+            length: 10,
+            width: 10
+        },
+        {
+            id: 3,
+            product_name: "Carrots",
+            picture_file: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2HxVHPSK60Jcnkyj8iMVdVYaGKhVn2yQFbw04KhUbxABQ3J1Jo5pjutgaAMiEER9K0mU&usqp=CAU",
+            available: false,
+            height: 5,
+            length: 5,
+            width: 5
+        }
+    ]
+};
+
+export default data;
