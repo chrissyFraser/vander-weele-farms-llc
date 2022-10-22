532bf099fa198bf13bdc58827d837c8da613eeb1
Joshua LaForest <josh.laforest38@gmail.com>
2022-10-22 13:36:46 -0800

    stillgettting db figured out

--- accounts_service/main.py
+++ accounts_service/main.py
@@ -14,7 +14,10 @@ app.include_router(authenticator.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
+    allow_origins=[
+        os.environ.get("CORS_HOST", "http://localhost:3000"),
+        os.environ.get("CORS_HOST", ["PUBLIC_URL"])
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
