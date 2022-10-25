b0d8d58191005ebf2fb88466aebe1828024b17aa
Steve <steve.svirko@gmail.com>
2022-10-25 15:35:21 -0400

    fixed get_all_customers unit test

--- monolith_service/tests/test_get_all_customers.py
+++ monolith_service/tests/test_get_all_customers.py
@@ -1,4 +1,6 @@
-from telnetlib import AUTHENTICATION
+
+from imaplib import _Authenticator
+
 from main import app
 from queries.customers import CustomerRepository, CustomerOut
 from fastapi.testclient import TestClient
@@ -18,14 +20,14 @@ class FakeGetAllCustomers:
 
 client = TestClient(app)
 headers = {
-    AUTHENTICATION: f"Bearer "
+    "Authorization": f"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMzk1Y2FlZC1hZDJkLTQ3NjEtODQyMi04MmExODQwMmFhNDAiLCJleHAiOjE2NjY3MjgyNTEsInN1YiI6InN0ZXZlLnN2aXJrb0BnbWFpbC5jb20iLCJhY2NvdW50Ijp7ImlkIjoiMSIsImVtYWlsIjoic3RldmUuc3ZpcmtvQGdtYWlsLmNvbSIsInVzZXJuYW1lIjoiYWRtaW4gc3RldmUiLCJoYXNoZWRfcGFzc3dvcmQiOiIkMmIkMTIkY0d1MTZLdDRhTW1ybDJRU3NHWHdkdTZGVkVMTnZ2czdBd0YubG13a29MNHIxcGtYVGNySzYifX0.IA9AoBoDvGU_Vgwm7FT-BP9rEQm9ly7MRJ5S4UKGe7k"
 }
 
 
 
 def test_get_customers():
     app.dependency_overrides[CustomerRepository] = FakeGetAllCustomers
-    response = client.get("/api/customers")
+    response = client.get("/api/customers", headers=headers)
     assert response.status_code == 200
     assert response.json() == get_accounts_list
     app.dependency_overrides = {}  
