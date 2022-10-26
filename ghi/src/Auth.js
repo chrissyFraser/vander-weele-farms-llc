ab4f92112eb90350d7914933329100810dd999da
Joshua LaForest <josh.laforest38@gmail.com>
2022-10-26 11:13:37 -0800

    removed await from auth

--- ghi/src/Auth.js
+++ ghi/src/Auth.js
@@ -10,11 +10,11 @@ export function getToken() {
 export async function getTokenInternal() {
   const url = `${process.env.REACT_APP_API_HOST}token/get`;
   try {
-    const response = await fetch(url, {
+    const response = fetch(url, {
       credentials: "include",
     });
     if (response.ok) {
-      const data = await response.json();
+      const data = response.json();
       internalToken = data.access_token;
       console.log(internalToken)
       return internalToken;
@@ -26,11 +26,11 @@ export async function getTokenInternal() {
 export async function getTokenData() {
   const url = `${process.env.REACT_APP_API_HOST}token/get`;
   try {
-    const response = await fetch(url, {
+    const response = fetch(url, {
       credentials: "include",
     });
     if (response.ok) {
-      const data = await response.json();
+      const data = response.json();
       let internalToken = data.token_type;
       return internalToken;
     }
@@ -83,7 +83,7 @@ export function useToken() {
 
   useEffect(() => {
     async function fetchToken() {
-      const token = await getTokenInternal();
+      const token = getTokenInternal();
       setToken(token);
     }
     if (!token) {
@@ -95,7 +95,7 @@ export function useToken() {
     if (token) {
       console.log("token found")
       const url = `${process.env.REACT_APP_API_HOST}token`;
-      await fetch(url, { method: "delete", credentials: "include" });
+      fetch(url, { method: "delete", credentials: "include" });
       internalToken = null;
       setToken(null);
       navigate("/");
@@ -109,18 +109,18 @@ export function useToken() {
     const form = new FormData();
     form.append("username", username);
     form.append("password", password);
-    const response = await fetch(url, {
+    const response = fetch(url, {
       method: "post",
       credentials: "include",
       body: form,
     });
     if (response.ok) {
 
-      const token = await getTokenInternal();
+      const token = getTokenInternal();
       setToken(token);
       return;
     }
-    let error = await response.json();
+    let error = response.json();
     return handleErrorMessage(error);
   }
 
