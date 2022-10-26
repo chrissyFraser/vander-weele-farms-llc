ae2bd5a5a4e27d43653932fa2531ccfabfe55646
Joshua LaForest <josh.laforest38@gmail.com>
2022-10-26 15:02:00 -0800

    fixed the bearer

--- monolith_service/routers/produce.py
+++ monolith_service/routers/produce.py
@@ -23,17 +23,17 @@ def get_all_produce(
 def create_produce(
     produce: Produce_create,
     queries: ProduceQueries = Depends(),
-    # account_data: dict = Depends(authenticator.get_current_account_data),
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ):
-    # if "admin" in account_data.get("username"):
-        # print("ACCOUNT DATA", account_data)
-    return queries.create_produce(produce)
-    # else:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_401_UNAUTHORIZED,
-    #         detail="Invalid token",
-    #         headers={"WWW-Authenticate": "Bearer"},
-    #     )
+    if "admin" in account_data.get("username"):
+        print("ACCOUNT DATA", account_data)
+        return queries.create_produce(produce)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
 
 @router.get("/api/produce/{produce_id}", response_model=Optional[Produce_get])
