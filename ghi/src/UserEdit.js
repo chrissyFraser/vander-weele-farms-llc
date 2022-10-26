bef87047951d1f9726f9174b35c8402094bac8f3
meganehrlich <meganehrlic4@gmail.com>
2022-10-26 15:16:57 -0700

    Fixing customer and merging

--- ghi/src/UserEdit.js
+++ ghi/src/UserEdit.js
@@ -1,49 +1,57 @@
 import { useState } from 'react';
 import { useToken, useAuthContext } from './Auth';
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom"; 
 
 console.log(useToken)
 
 function UserUpdateComponent() {
-    const navigate = useNavigate();
+
+    // const navigate = useNavigate();
+
+
+        
 
     async function updateUser(customer_name, customer_address, customer_email, driver_id, priority_id) {
 
 
-        const murl = `${process.env.REACT_APP_API_HOST_MONOLITH}/api/customers`;
-        const response = await fetch(murl, {
-            method: "post",
-            body: JSON.stringify({
-                customer_name,
-                customer_address,
-                customer_email,
-                driver_id,
-                priority_id
+      
 
-            }),
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            },
+
+
+        const url = `${process.env.REACT_APP_API_HOST_MONOLITH}/api/customers`;
+        const response = await fetch(url, {
+        method: "post",
+        body: JSON.stringify({
+            customer_name, 
+            customer_address,
+            customer_email,
+            driver_id,
+            priority_id
+            
+        }),
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
         });
         if (response.ok) {
-            response.status_code = 200
-            navigate("/cart")
-
-        }
-
-
+        response.status_code = 200
+        
+        // await login(username, password);
+        } 
+            
+        
         return false;
     }
 
     const { token } = useAuthContext();
-    function parseJwt(token) {
+    function parseJwt (token) {
         var base64Url = token.split('.')[1];
         var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
-
+    
         return JSON.parse(jsonPayload);
     }
     const data = parseJwt(token)
@@ -57,10 +65,10 @@ function UserUpdateComponent() {
     let [customer_address, setPassword] = useState('')
 
     const submitHandler = e => {
-
+        
         updateUser(userName, customer_address, userEmail)
         e.preventDefault();
-        console.log("Form information", userName, customer_address, userEmail)
+        console.log(userName, customer_address, userEmail)
 
     }
 
@@ -68,10 +76,9 @@ function UserUpdateComponent() {
     return (
         <div>
             <center>
-                <h2>Please enter your address for orders</h2>
                 <form onSubmit={submitHandler}>
-                    <input type="text" name="email" placeholder="Email" value={userEmail} /><br />
                     <input type="text" name="customer_name" placeholder="Name" value={userName} /><br />
+                    <input type="text" name="email" placeholder="Email" value={userEmail} /><br />
                     <input type="text" name="customer_address" placeholder="Address" value={customer_address} onChange={(event) => setPassword(event.target.value)} /><br />
                     <input type="submit" name="submit" />
                 </form>
