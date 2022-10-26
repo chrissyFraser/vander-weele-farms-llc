68a65894d5c012f059346ddf08094ef0d3dbd3a2
Joshua LaForest <josh.laforest38@gmail.com>
2022-10-25 20:15:00 -0800

    changed the db_url in migrations/__main__ and added the account migration

--- monolith_service/migrations/__main__.py
+++ monolith_service/migrations/__main__.py
@@ -3,7 +3,7 @@ async def migrate():
     import os
     import sys
 
-    db_url = os.environ["DATABASE_URL"]
+    db_url = 'https://vanderweele-farms-llc.herokuapp.com'
 
     if len(sys.argv) < 2:
         print("Command: up|down [amount]")
