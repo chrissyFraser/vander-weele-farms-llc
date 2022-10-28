59ca70753866b19faea18a05665483402526fd0c
Steve <steve.svirko@gmail.com>
2022-10-27 21:08:45 -0400

    formatting document

--- ghi/src/ProduceItem.js
+++ ghi/src/ProduceItem.js
@@ -1,107 +1,124 @@
-import { useEffect, useState } from 'react';
-import { useAuthContext } from './Auth';
+import { useEffect, useState } from "react";
+import { useAuthContext } from "./Auth";
 // removed BrowserRouter, NavLink, Route, Routes from this import
 import { useNavigate } from "react-router-dom";
 
-
-
 function ProduceItem(props) {
-    const [item, setItem] = useState([]);
-    console.log("item", item)
-    // const [delete, setDelete] = useState([]);
-    useEffect(() => {
-        async function getProduceItem() {
-            let url = `${process.env.REACT_APP_API_HOST_MONOLITH}/api/produce/${props.produce_id}`;
-            let response = await fetch(url);
-            let data = await response.json();
-            if (response.ok) {
-                setItem(data);
-            }
-        }
-        getProduceItem();
-        //eslint-disable-next-line
-    }, [])
+  const [item, setItem] = useState([]);
+  console.log("item", item);
+  // const [delete, setDelete] = useState([]);
+  useEffect(() => {
+    async function getProduceItem() {
+      let url = `${process.env.REACT_APP_API_HOST_MONOLITH}/api/produce/${props.produce_id}`;
+      let response = await fetch(url);
+      let data = await response.json();
+      if (response.ok) {
+        setItem(data);
+      }
+    }
+    getProduceItem();
+    //eslint-disable-next-line
+  }, []);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleDelete = e => {
-        fetch(`${process.env.REACT_APP_API_HOST_MONOLITH}/api/produce/${props.produce_id}/delete`, {
-            method: "DELETE",
-            headers: { "Content-Type": "application/json",
-                    "Authorization": `Bearer ${token}` },
-        }).then(() => {
-            console.log("Produce deleted")
-            navigate('/produce-admin');
-            window.location.reload();
+  const handleDelete = (e) => {
+    fetch(
+      `${process.env.REACT_APP_API_HOST_MONOLITH}/api/produce/${props.produce_id}/delete`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    ).then(() => {
+      console.log("Produce deleted");
+      navigate("/produce-admin");
+      window.location.reload();
+    });
+  };
+  function parseJwt(token) {
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+      window
+        .atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
         })
-    };
-    function parseJwt(token) {
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
+        .join("")
+    );
 
-        return JSON.parse(jsonPayload);
+    return JSON.parse(jsonPayload);
+  }
+  const { token } = useAuthContext();
+  if (token) {
+    const data = parseJwt(token);
+    console.log("DATA", Object.entries(data));
+    console.log("specific", Object.values(data));
+    const user = Object.values(data);
+    console.log(user[3]);
+    const myUser = user[3];
+    const valuesUser = Object.values(myUser);
+    console.log(valuesUser[0]);
+    const user_status = valuesUser[2];
+    if (user_status.toLowerCase().includes("admin")) {
+      return (
+        <table className="table is-striped">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Available</th>
+              <th>Picture</th>
+              <th>Length</th>
+              <th>Height</th>
+              <th>Width</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{item.product_name}</td>
+              <td className="has-text-centered">
+                {item.available ? "Yes" : "No"}
+              </td>
+              <td>
+                <img
+                  src={`https://vwimageuploads.s3.us-west-2.amazonaws.com/${item.picture_file}`}
+                  id="image"
+                  alt="Thumbnail"
+                  className="user-post"
+                  width={100}
+                />
+              </td>
+              <td>{item.length}</td>
+              <td>{item.height}</td>
+              <td>{item.width}</td>
+              <td>
+                <button
+                  type="button"
+                  className="btn btn-warning"
+                  id="delete_item button"
+                  onClick={() =>
+                    handleDelete(`/produce-admin/${item.produce_id}`)
+                  }
+                >
+                  Delete Item
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      );
     }
-    const { token } = useAuthContext();
-    if (token) {
-        const data = parseJwt(token)
-        console.log("DATA", Object.entries(data))
-        console.log("specific", Object.values(data))
-        const user = Object.values(data)
-        console.log(user[3])
-        const myUser = user[3]
-        const valuesUser = Object.values(myUser)
-        console.log(valuesUser[0])
-        const user_status = valuesUser[2]
-        if (user_status.toLowerCase().includes("admin")) {
-            return (
-                <table className="table is-striped">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Available</th>
-                            <th>Picture</th>
-                            <th>Length</th>
-                            <th>Height</th>
-                            <th>Width</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{item.product_name}
-                            </td>
-                            <td className="has-text-centered">
-                                {item.available ? 'Yes' : 'No'}
-                            </td>
-                            <td>
-                                <img
-                                    src={`https://vwimageuploads.s3.us-west-2.amazonaws.com/${item.picture_file}`}
-                                    id="image"
-                                    alt="Thumbnail"
-                                    className="user-post"
-                                    width={100}
-                                />
-                            </td>
-                            <td>{item.length}</td>
-                            <td>{item.height}</td>
-                            <td>{item.width}</td>
-                            <td><button type="button" className="btn btn-warning" id="delete_item button"
-                                onClick={() => handleDelete(`/produce-admin/${item.produce_id}`)}>Delete Item</button></td>
-                        </tr>
-                    </tbody>
-                </table>
-            )
-        }
-    } else {
-        return (
-            <>
-                <h2>ou do not have access to this page.</h2>
-            </>
-        )
-    }
-
+  } else {
+    return (
+      <>
+        <h2>ou do not have access to this page.</h2>
+      </>
+    );
+  }
 }
-export default ProduceItem
+export default ProduceItem;
--- ghi/src/components/Basket.js
+++ ghi/src/components/Basket.js
@@ -1,144 +1,147 @@
 import React from "react";
-import { useState } from 'react';
-import {  useAuthContext } from '../Auth';
+import { useState } from "react";
+import { useAuthContext } from "../Auth";
 // import Alert from 'react-bootstrap/Alert';
 import { useNavigate } from "react-router-dom";
 // import datetime
 
 export default function Basket(props) {
-    console.log("cart", props)
-    const { cartItems, onAdd, onRemove } = props;
-    const [data] = useState();
-    // const [product_name, setProduct] = useState();
-    // const [qty, setQty] = useState();
+  console.log("cart", props);
+  const { cartItems, onAdd, onRemove } = props;
+  const [data] = useState();
+  // const [product_name, setProduct] = useState();
+  // const [qty, setQty] = useState();
 
+  const { token } = useAuthContext();
+  function parseJwt(token) {
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+      window
+        .atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
 
+    return JSON.parse(jsonPayload);
+  }
 
-    const { token } = useAuthContext();
-        function parseJwt (token) {
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-    
-        return JSON.parse(jsonPayload);
-    }
+  // function cartAlert() {
+  //     return (
+  //         <>
+  //         {['success',
+  //         ].map((variant) => (
+  //             <Alert key={variant} variant={variant}>
+  //             Thank you for your order!
+  //             </Alert>
+  //         ))}
+  //         </>
+  //     );
+  //     }
 
-        // function cartAlert() {
-        //     return (
-        //         <>
-        //         {['success',
-        //         ].map((variant) => (
-        //             <Alert key={variant} variant={variant}>
-        //             Thank you for your order!
-        //             </Alert>
-        //         ))}
-        //         </>
-        //     );
-        //     }
+  const navigate = useNavigate();
 
-    
-    const navigate = useNavigate();
+  const handleClick = async () => {
+    // cartItems.map((item) => setProduct(item.product_name))
+    // cartItems.map((item) => setQty(item.qty))
+    // console.log(product_name)
+    // console.log(qty)
 
-    const handleClick = async () => {
-        
-        
-    
-        // cartItems.map((item) => setProduct(item.product_name))
-        // cartItems.map((item) => setQty(item.qty))
-        // console.log(product_name)
-        // console.log(qty)
+    const data1 = parseJwt(token);
+    const user = Object.values(data1);
+    const myUser = user[3];
+    const valuesUser = Object.values(myUser);
 
-        
-        const data1 = parseJwt(token)
-        const user = Object.values(data1)
-        const myUser = user[3]
-        const valuesUser = Object.values(myUser)
+    const num1 = cartItems.map((item) => item.id);
+    const num2 = cartItems.map((item) => item.qty);
+    const produce_id = num1[0];
+    const qty = num2[0];
+    const printed = false;
+    const driver_id = 0;
+    const od = Date.now();
+    const order_date = new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    }).format(od);
+    const customer_id = parseInt(valuesUser[0]);
 
-        const num1 = cartItems.map((item) => (item.id))
-        const num2 = cartItems.map((item) => (item.qty))
-        const produce_id = num1[0]
-        const qty = num2[0]
-        const printed = false
-        const driver_id = 0
-        const od = Date.now()
-        const order_date = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(od)
-        const customer_id = parseInt(valuesUser[0])
+    console.log("customer_id", customer_id);
+    console.log("date", order_date);
+    console.log("id", produce_id);
+    console.log("qty", qty);
 
-        console.log("customer_id", customer_id)
-        console.log("date", order_date)
-        console.log("id", produce_id)
-        console.log("qty", qty)
+    const data = {
+      customer_id,
+      produce_id,
+      qty,
+      driver_id,
+      order_date,
+      printed,
+    };
 
-        const data = {
-                customer_id,
-                produce_id,
-                qty,
-                driver_id,
-                order_date,
-                printed
-        };
+    console.log("data", data);
 
-        console.log("data", data);
+    fetch(`${process.env.REACT_APP_API_HOST_MONOLITH}/api/orders`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(() => {
+      navigate("/");
+    });
 
-        fetch(`${process.env.REACT_APP_API_HOST_MONOLITH}/api/orders`, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json',
-                "Authorization": `Bearer ${token}`
-            },
-            
-        }).then(() =>{
-            navigate("/")
-        }
-        )
+    // const result = await response.json();
 
-        // const result = await response.json();
+    // console.log('result is: ', JSON.stringify(result));
 
-        // console.log('result is: ', JSON.stringify(result));
+    // setData(result);
+    // // cartAlert();
+    // navigate("/")
+  };
 
-        // setData(result);
-        // // cartAlert();
-        // navigate("/")
-    
-};
+  console.log(data);
 
-console.log(data);
-
-
-    return (
-        <aside className="block col-1">
-            <h2>Basket</h2>
-            {cartItems.length === 0 && <div>Basket is Empty</div>}
-            {cartItems.map((item) => (
-                <div key={item.id} className="row">
-                    <div className="col-2">{item.product_name}</div>
-                    <div className="col-2">
-                        <button onClick={() => onRemove(item)} className="remove">-</button>
-                        <button onClick={() => onAdd(item)} className="add">+</button>
-                    </div>
-                    <div className="col-2">
-                        {item.qty}
-                    </div>
-                </div>
-            ))}
-            {cartItems.length !== 0 && (
-                <>
-                    <hr></hr>
-                    <div className="row"></div>
-                    <div className="col-2">Total Quantity</div>
-                    <div className="col-1">{cartItems.reduce((a, v) => a = a + v.qty, 0)}</div>
-                    <hr />
-                    <div className="row">
-                        <button onClick={handleClick}>
-                            Submit
-                        </button>
-                    </div>
-
-                </>
-            )}
-        </aside>
-);
+  return (
+    <aside className="block col-1">
+      <h2>Basket</h2>
+      {cartItems.length === 0 && <div>Basket is Empty</div>}
+      {cartItems.map((item) => (
+        <div key={item.id} className="row">
+          <div className="col-2">{item.product_name}</div>
+          <div className="col-2">
+            <button onClick={() => onRemove(item)} className="remove">
+              -
+            </button>
+            <button onClick={() => onAdd(item)} className="add">
+              +
+            </button>
+          </div>
+          <div className="col-2">{item.qty}</div>
+        </div>
+      ))}
+      {cartItems.length !== 0 && (
+        <>
+          <hr></hr>
+          <div className="row"></div>
+          <div className="col-2">Total Quantity</div>
+          <div className="col-1">
+            {cartItems.reduce((a, v) => (a = a + v.qty), 0)}
+          </div>
+          <hr />
+          <div className="row">
+            <button onClick={handleClick}>Submit</button>
+          </div>
+        </>
+      )}
+    </aside>
+  );
 }
